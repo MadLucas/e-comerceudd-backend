b8addc9bbd24d7565518001de4ba85cf364f44f8
Milenaandrews <mileandrews@gmail.com>
2023-12-26 18:07:49 -0300

    ultimos cambios

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -10,8 +10,8 @@ productRouter.route('/products')
     .post( productController.createProduct); // Crear un nuevo producto
 
 productRouter.route('/products/:id')
-    .get(auth, productController.getProductById) // Obtener un producto por ID
-    .put(auth, productController.updateProduct) // Actualizar un producto por ID
-    .delete(auth, productController.deleteProduct); // Eliminar un producto por ID
+    .get(productController.getProductById) // Obtener un producto por ID
+    .put(productController.updateProduct) // Actualizar un producto por ID
+    .delete( productController.deleteProduct); // Eliminar un producto por ID
 
 module.exports = productRouter;
