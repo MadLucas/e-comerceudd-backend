0b494d5d4877f574f181b291b772fcb5ec3edc3a
Lucas Fernandez <lucasfernandezc@outlook.com>
2024-01-07 18:08:46 -0300

    bd

--- models/Product.js
+++ models/Product.js
@@ -48,6 +48,6 @@ const productSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model("programingbooks", productSchema);
 
 module.exports = Product;
